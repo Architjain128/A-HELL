ddd393a37b628a6a83ed82326496b449c298fe52
Architjain128 <architjain10202@gmail.com>
2020-09-26 19:51:39 +0530

    ll

--- env.c
+++ env.c
@@ -1,46 +1,5 @@
 #include "header.h"
 
-void callingsetenv(char* str)
-{
-    char *setinp[10];
-    char set2inp[10000];
-    const char setdelimiters[] = " \t\0";
-    char *totken = strtok(str, setdelimiters);
-    ll i=0;
-    while (totken != NULL)
-    {
-        if(strcmp(totken,"setenv")!=0)
-        {
-            setinp[i]=totken;
-            if(i!=0)
-            {
-                if(i==1)
-                strcpy(set2inp,totken);
-                else
-                {
-                    strcat(set2inp," ");
-                    strcat(set2inp,totken);
-                }
-            }
-            i++;
-        }
-        totken = strtok(NULL, setdelimiters);
-    }
-   
-    if(i<2)
-    {
-        printf("\033[0;33m--> WARNING : value is NULL \033[0m\n");
-        strcpy(setinp[1],"");
-    }
-    else if(setenv(setinp[0],set2inp,1)==-1)
-    {
-        printf("\033[0;33m--> ERROR : setenv failed \033[0m\n");
-        perror(";(");
-        exit_fail=1;
-        exit(EXIT_FAILURE);
-    }
-}
-
 void callingunsetenv(char* str)
 {
     int unset_flag_arg=0;
@@ -74,5 +33,52 @@ void callingunsetenv(char* str)
             perror(";(");
             exit_fail=1;
         }
+    }
+}
+
+void callingsetenv(char* str)
+{
+    char *setinp[10];
+    char set2inp[10000];
+    char zalima[10000];
+    const char setdelimiters[] = " \t\0";
+    char *totken = strtok(str, setdelimiters);
+    ll i=0;
+    while (totken != NULL)
+    {
+        if(strcmp(totken,"setenv")!=0)
+        {
+            if(i==0)
+            setinp[0]=totken;
+
+            else
+            {
+                if(i==1)
+                strcpy(set2inp,totken);
+                else
+                {
+                    strcat(set2inp," ");
+                    strcat(set2inp,totken);
+                }
+            }
+            i++;
+        }
+        totken = strtok(NULL, setdelimiters);
+    }
+   
+    if(i<2)
+    {
+        printf("\033[0;33m--> WARNING : value is NULL \033[0m\n");
+        stpcpy(zalima,"unsetenv ");
+        strcat(zalima,setinp[0]);
+        callingunsetenv(zalima);
+        strcpy(set2inp," ");
+    }
+    if(setenv(setinp[0],set2inp,1)==-1)
+    {
+        printf("\033[0;33m--> ERROR : setenv failed \033[0m\n");
+        perror(";(");
+        exit_fail=1;
+        exit(EXIT_FAILURE);
     }
 }
